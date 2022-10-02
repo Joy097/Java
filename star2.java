9c35fd2431868d9a3f32bf8e5c356abf41018f88
Joy097 <69787432+Joy097@users.noreply.github.com>
2022-10-02 12:03:19 +0600

    Add files via upload

---
+++ star2.java
@@ -0,0 +1,17 @@
+public class stargod{
+  public static void main(String[]args){
+    int i;
+    int j;
+    for(i=1;i<=4;i++)
+    {for(j=4;j>=i;j--)
+      { System.out.print("U");
+    }
+    System.out.println();
+    }
+    for(i=1;i<=4;i++)
+    {for(j=1;j<=i;j++)
+      { System.out.print("U");
+    }
+    System.out.println();
+    }
+  }}
